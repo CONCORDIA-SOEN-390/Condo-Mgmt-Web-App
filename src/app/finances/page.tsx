b40a7302077060c3806772bbfe7ad4b247b32958
Fadi <87549228+Lukateki@users.noreply.github.com>
2024-03-19 18:46:57 -0400

    finance page conditional rendering + company specific finance components

--- src/app/finances/page.tsx
+++ src/app/finances/page.tsx
@@ -1,20 +1,25 @@
 "use client";
 import { MdEditSquare } from "react-icons/md";
 import { MdCancel } from "react-icons/md";
-import SideBar from "@/components/GeneralComponents/PublicUserView/SideBar";
+import PublicUserSideBar from "@/components/GeneralComponents/PublicUserView/SideBar";
+import CompanySideBar from "@/components/GeneralComponents/CompanyView/SideBar";
 import CardHeader from "@/components/GeneralComponents/CardHeader";
-import FinanceTable from "@/components/FinancePageComponents/PublicUserView/FinanceTable";
+import PublicUserFinanceTable from "@/components/FinancePageComponents/PublicUserView/FinanceTable";
+import CompanyFinanceTable from "@/components/FinancePageComponents/CompanyView/FinanceTable";
 
 function page() {
+  const page = 'company';
   return (
     <div className="flex min-h-screen-nav items-center justify-center h-full bg-white">
-      <SideBar page='finances'/>
+      {page === 'company'? <CompanySideBar page='finances'/>:<PublicUserSideBar page='finances'/>}
+      
       <div className="absolute w-5/6 right-6 top-5 bottom-5 bg-white shadow-lg rounded-xl">
         <CardHeader title="Finances">
           <MdEditSquare className="" />
           <MdCancel className="" />
         </CardHeader>
-        <FinanceTable/>
+        {page === 'company'? <CompanyFinanceTable/>:<PublicUserFinanceTable/>}
+        
       </div>
     </div>
   );

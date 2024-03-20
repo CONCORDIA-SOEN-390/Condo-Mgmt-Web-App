d3e3b822f969808fe4f9bc8db6d88fc71f04938f
kara <racha.kara@gmail.com>
2024-03-20 01:43:59 -0400

    Update Footer
    - make font darker
    - removed "About"

--- src/components/GeneralComponents/Footer.tsx
+++ src/components/GeneralComponents/Footer.tsx
@@ -7,10 +7,7 @@ const Footer = () => {
         <footer className="flex w-full bottom-0 left-0 z-20 p-4 bg-sky-50 mt-10 mb-0 border-0 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-white-800 dark:border-white-600">
             <span className="text-sm text-black sm:text-center dark:text-gray-400">© 2024 <a href="/" className="hover:underline">Condo360™</a>. All Rights Reserved.
             </span>
-            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-black dark:text-gray-400 sm:mt-0">
-                <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
-                </li>
+            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-black dark:text-gray-500 sm:mt-0">
                 <li>
                     <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
                 </li>

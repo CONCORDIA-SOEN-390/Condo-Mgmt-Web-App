2605ee0cbf3586a00c330ba06c14a878f210480e
kara <racha.kara@gmail.com>
2024-03-20 00:53:31 -0400

    Update NavBar
    - changed the font size
    - changed the background color
    - Deleted the Properties link

--- src/components/GeneralComponents/NavBar.tsx
+++ src/components/GeneralComponents/NavBar.tsx
@@ -4,24 +4,21 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex w-full overflow-hidden fixed top-0 mt-5 bg-[#90c5fd00] transition-colors duration-200">
-      <div className="text-[#332d2d] text-1.7rem font-bold pt-4 text-center ml-8">
+    <div className="flex w-full overflow-hidden fixed top-0 pb-1 bg-gradient-to-b from-sky-50 to-transparent">
+      <div className="text-[#332d2d] text-2xl font-bold pt-5 text-center ml-8">
         <a href="/">Condo360</a>
       </div>
 
-      <div className="text-center mx-auto ml-[360px]">
+      <div className="text-center mx-auto ml-[500px]">
         <ul className="flex list-none m-0 p-0 w-full">
           <li>
-            <Link href="/" className="block text-[#332d2d] text-center px-10 py-5 font-semibold text-1.4rem hover:font-bold">Home</Link>
+            <Link href="/" className="block text-[#332d2d] text-center px-10 py-6 font-semibold text-xl hover:font-bold">Home</Link>
           </li>
           <li>
-            <Link href="/properties" className="block text-[#332d2d] text-center px-10 py-5 font-semibold text-1.4rem hover:font-bold">Properties</Link>
+            <Link href="#" className="block text-[#332d2d] text-center px-10 py-6 font-semibold text-xl hover:font-bold">Contact Us</Link>
           </li>
           <li>
-            <Link href="#" className="block text-[#332d2d] text-center px-10 py-5 font-semibold text-1.4rem hover:font-bold">Contact</Link>
-          </li>
-          <li>
-            <Link href="/login" className="block text-[#332d2d] text-center px-10 py-5 font-semibold text-1.4rem hover:font-bold">Sign In</Link>
+            <Link href="/login" className="block text-[#332d2d] text-center px-10 py-6 font-semibold text-xl hover:font-bold">Sign In</Link>
           </li>
         </ul>
       </div>

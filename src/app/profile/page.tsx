5dbb033c89e83eff546328c23bcdbaa59e2a8f54
Imane Madda <104864748+imanemadda@users.noreply.github.com>
2024-03-20 18:39:58 -0400

    Added state for profile picture path

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,9 +3,10 @@ import React from "react";
 import SideBar from "@/components/GeneralComponents/PublicUserView/SideBar";
 
 function Page() {
-  const [name, setName] = React.useState("John Smith");
-  const [email, setEmail] = React.useState("john_smith@gmail.com");
-  const [phoneNumber, setPhoneNumber] = React.useState("(514) 123-4567");
+  const [profilePic] = React.useState("/TemporaryProfilePicture.png"); // Remove TemporaryProfilePicture from profile folder
+  const [name] = React.useState("John Smith");
+  const [email] = React.useState("john_smith@gmail.com");
+  const [phoneNumber] = React.useState("(514) 123-4567");
 
   return (
     <div className="flex min-h-screen-nav items-center justify-center h-full bg-white">
@@ -14,8 +15,8 @@ function Page() {
         <div className="row-start-2 row-end-2 col-start-1 col-end-2 flex items-center justify-center">
           <div className="w-40 h-40">
             <img
-              src={"/TemporaryProfilepicture.png"}
-              alt="Profile Picture"
+              src= {profilePic}
+              alt= "Profile Picture"
               className="w-full h-full object-cover full"
             />
           </div>

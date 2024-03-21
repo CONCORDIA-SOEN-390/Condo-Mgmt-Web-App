954fcfc12b9734e794d37e1ece176639b199eb5d
kara <racha.kara@gmail.com>
2024-03-21 03:00:49 -0400

    Signup pages styling
    - added styling to both signup pages

--- src/app/(auth)/signup/complete/page.tsx
+++ src/app/(auth)/signup/complete/page.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import React from "react";
 import ProfileCompletionForm from "@/components/CompleteProfileComponents/ProfileCompletionForm";
+import NavBar from "@/components/GeneralComponents/NavBar";
+import Footer from "@/components/GeneralComponents/Footer";
+import "@/app/styling/style.css";
 
 function completeSignup() {
   return (
-    <ProfileCompletionForm/>
+    <div className="container-blurry">
+      <div>
+        <NavBar/>
+        <div className="bg-sky-50/[0.75] my-36 py-10 rounded-lg mx-auto max-w-xl">
+          <ProfileCompletionForm/>
+        </div>
+        <Footer/>
+        </div>
+    </div>
   );
 }
 

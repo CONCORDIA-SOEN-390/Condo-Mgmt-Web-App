14253e1195bc8f9705faf004e0babeb6faf2c44e
kara <racha.kara@gmail.com>
2024-03-20 23:20:33 -0400

    Login page Styling
    - added styling to the login page and login form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,18 +1,27 @@
 import React from "react";
-import LoginForm from "@/components/LoginPageComponents/LoginForm";
 import Link from "next/link";
+import NavBar from "@/components/GeneralComponents/NavBar";
+import LoginForm from "@/components/LoginPageComponents/LoginForm";
+import Footer from "@/components/GeneralComponents/Footer";
+import "@/app/styling/style.css";
 
 function Login() {
   return (
-    <>
-      <LoginForm />
-      <p className="mt-10 text-center text-sm text-gray-500">
-        Not a member?
-        <Link href="/signup" className="font-semibold leading-6 ml-2 text-indigo-600 hover:text-indigo-500">
-          Sign Up
-        </Link>
-      </p>
-    </>
+    <div className="container-blurry">
+      <div>
+        <NavBar/>
+        <div className="bg-sky-50/[0.75] my-36 py-10 rounded-lg mx-auto max-w-2xl">
+          <LoginForm />
+          <p className="mt-6 text-center text-sm text-gray-500">
+            Not a member? 
+            <Link href="/signup" className="ml-2 font-semibold leading-6 text-yellow-800 hover:text-yellow-600">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+        <Footer/>
+      </div>
+    </div>
   );
 }
 

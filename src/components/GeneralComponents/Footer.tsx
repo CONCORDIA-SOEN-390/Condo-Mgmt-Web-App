7809dd8c411480f27974973dfd3aef5c5a5abf4c
kara <racha.kara@gmail.com>
2024-03-21 03:53:24 -0400

    NavBar & Footer Update
    - adjusted the styling for the to stick at the bottom
    - centered the navbar links (were off because of the logo)

--- src/components/GeneralComponents/Footer.tsx
+++ src/components/GeneralComponents/Footer.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 const Footer = () => {
 
     return (
-        <footer className="flex w-full bottom-0 left-0 z-20 p-4 bg-sky-50 mt-10 mb-0 border-0 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-white-800 dark:border-white-600">
+        <footer className="relative w-full bottom-0 left-0 z-20 p-4 bg-sky-50 mt-10 mb-0 border-0 shadow md:flex md:items-center md:justify-between md:p-6">
             <span className="text-sm text-black sm:text-center dark:text-gray-400">© 2024 <a href="/" className="hover:underline">Condo360™</a>. All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-black dark:text-gray-500 sm:mt-0">

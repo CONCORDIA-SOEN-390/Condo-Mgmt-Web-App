ef64fb1bbaceaa860e295311974dc0087b1c5708
BoltyZeus <74887519+BoltyZeus@users.noreply.github.com>
2024-04-08 02:53:36 -0400

    #200: Register parking

---
+++ next_project/src/app/api/handleParkingRegistration/route.js
@@ -0,0 +1,36 @@
+import pool from "../../../../utils/db";
+
+export async function POST(req) {
+    const body = await req.json();
+    const { propertyId, parkingOwnerId } = body;
+
+    const client = await pool.connect();
+
+    try {
+        //await client.query("BEGIN"); // Start the transaction
+
+        const p = await client.query("SELECT * FROM parking WHERE occupied = false AND property_id = $1 LIMIT 1", [propertyId])
+
+        if (p.rows.length === 0){
+            return new Response('All spaces filled', {
+                status: 400
+            });
+        }
+        const parking_id = p.rows[0]['parking_id'];
+        // update the request status
+        await client.query("UPDATE parking SET owner_id = $1, occupied = true WHERE parking_id = $2", [parkingOwnerId, parking_id]);
+
+        await client.query("COMMIT"); // commit the transaction
+        return new Response('Status updated successfully', {
+            status: 200
+        });
+
+    } catch (error) {
+        await client.query("ROLLBACK"); // rollback the transaction if an error occurs
+        return new Response('Internal Server Error', {
+            status: 500,
+        });
+    } finally {
+        client.release();
+    }
+}

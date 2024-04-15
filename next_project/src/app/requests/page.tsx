d05217632ff4f7411107a209b6c9e7d826771c6b
Vanisha Patel <p_vanish@live.concordia.ca>
2024-04-15 01:26:32 -0400

    fixing page rendering

--- next_project/src/app/requests/page.tsx
+++ next_project/src/app/requests/page.tsx
@@ -1,61 +1,34 @@
-
+"use client";
+import React from "react";
 import PublicUserSideBar from "@/components/GeneralComponents/PublicUserView/SideBar";
 import CompanySideBar from "@/components/GeneralComponents/CompanyView/SideBar";
 import CardHeader from "@/components/GeneralComponents/CardHeader";
 import RequestCompanyView from "@/components/RequestPageComponents/CompanyView/Request";
-import {auth} from "@/lib/auth";
+import RequestPublicView from "@/components/RequestPageComponents/PublicUserView/Request";
+import RequestEmployeeView from "@/components/RequestPageComponents/EmployeeView/RequestTable";
+import { useSession } from "next-auth/react";
 
-async function RequestsPage() {
-    const session = await auth()
-  // @ts-ignore comment
-    const {account_type:page} = session?.user;  
-
-
-    //------------------------------------FIX PAGE RENDERING HERE------------------------------------
-    //const page = 'company';
-    // harcoded userId
-    //const userId = 13; // employee user   <RequestEmployeeView userId={userId}/>
-    //const userId = 1;   // company user  <RequestCompanyView userId={userId}/>
-    //const userId = 19;   // condo owner  <RequestPublicView userId={userId} />
-
-    //-----------------------------------------------------------------------------------------------
-
-
-    // // toggle for add and edit
-    // const [showAddRequestForm, setshowAddRequestFormFormStatus] = useState(false);
-    // // const toggleFormAdd = () => {
-    // //     setshowAddRequestFormFormStatus(!showAddRequestForm);
-    // // };
-
-    // const [showEditRequestForm, setshowEditRequestFormFormStatus] = useState(false);
-    // // const toggleFormEdit = () => {
-    // //     setshowEditRequestFormFormStatus(!showEditRequestForm);
-    // // };
+function RequestsPage() {
+    const { data: session } = useSession();
+    // @ts-ignore comment
+    const userId = session?.user?.user_id;
+    // @ts-ignore comment
+    const page = session?.user?.account_type;
+    // @ts-ignore comment
 
     return (
         <div className="flex min-h-screen-nav items-center justify-center h-full bg-white">
-            {/*fix rendering here**/}
-            {page === 'company'? <CompanySideBar page='requests'/>:<PublicUserSideBar page='requests'/>}
+            {page === 'company' ? <CompanySideBar page={page}/> : <PublicUserSideBar page={page}/>}
             <div className="absolute w-5/6 right-6 top-5 bottom-5 bg-white shadow-lg rounded-xl">
-                <CardHeader title="Assigned Requests">
+                <CardHeader title="Requests">
                     .
                 </CardHeader>
-
-                {/*fix rendering here
-                there are three different types of pages
-                */}
-
-                {/*
-<RequestPublicView userId={userId} />
-
-<RequestEmployeeView userId={userId}/>
-
-                */}
-
-                <RequestCompanyView />
-
+                {page === 'company' && <RequestCompanyView userId={userId} />}
+                {page === 'reg_user' && <RequestPublicView userId={userId} />}
+                {page !== 'company' && page !== 'reg_user' && <RequestEmployeeView userId={userId} />}
             </div>
         </div>
     );
 }
+
 export default RequestsPage;

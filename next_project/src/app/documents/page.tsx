718c4cae6fc4c3c291d1a33e8c76a945f84a8c36
Vanisha Patel <p_vanish@live.concordia.ca>
2024-04-30 00:27:58 -0400

    profile page with notification

--- next_project/src/app/documents/page.tsx
+++ next_project/src/app/documents/page.tsx
@@ -19,7 +19,7 @@ export default function DocumentsPage() {
 
     return (
         <div className="flex min-h-screen-nav items-center justify-center h-full bg-white">
-            {page === 'company' ? <CompanySideBar page='properties'/> : <PublicUserSideBar page='properties'/>}
+            {page === 'company' ? <CompanySideBar page='documents'/> : <PublicUserSideBar page='properties'/>}
             <div className="absolute w-5/6 inset-y-0 right-5 bg-white shadow-lg rounded-xl">
                 <CardHeader title="Documents">
                     .
--- next_project/src/app/profile/page.tsx
+++ next_project/src/app/profile/page.tsx
@@ -4,44 +4,29 @@ import SideBar from "@/components/GeneralComponents/PublicUserView/SideBar";
 import {useSession} from "next-auth/react";
 import RegisterForm from "@/components/ProfilePageComponents/RegistrationForm";
 import {auth} from "@/lib/auth";
+import CompanySideBar from "@/components/GeneralComponents/CompanyView/SideBar";
+import PublicUserSideBar from "@/components/GeneralComponents/PublicUserView/SideBar";
+import CardHeader from "@/components/GeneralComponents/CardHeader";
+
+import Dashboard from "@/components/ProfilePageComponents/Dashboard";
 
 function ProfilePage() {
     const { data: session } = useSession();
     // @ts-ignore comment
     const userId = session?.user?.user_id;
     // @ts-ignore comment
-    const userName = session?.user?.username;
-    // @ts-ignore comment
-    const email = session?.user?.email;
-    // @ts-ignore comment
-    const phoneNumber = session?.user?.phone_number;
+    const page = session?.user?.account_type;
 
   return (
       <div className="flex min-h-screen-nav items-center justify-center h-full bg-white">
-        <SideBar page="profile" />
-        <div className="relative grid grid-rows-4 grid-cols-1 gap-4 p-5 text-black text-xl justify-center items-center">
-          <div className="row-start-2 row-end-2 col-start-1 col-end-2 flex items-center justify-center">
-            <div className="w-40 h-40">
-
-            </div>
+          {page === 'company' ? <CompanySideBar page='profile'/> : <PublicUserSideBar page='properties'/>}
+          <div className="absolute w-5/6 inset-y-0 right-5 bg-white shadow-lg rounded-xl">
+              <CardHeader title="Profile">
+                  .
+              </CardHeader>
+              <Dashboard userId={userId} />
           </div>
-          <div className="row-start-3 mrow-end-4 col-start-1 col-end-2">
-            <div className="col-start-2 col-end-3 p-2">
-              <strong>Name: </strong> {userName}
-            </div>
-            <div className="col-start-2 col-end-3 p-2">
-              <strong>Email: </strong> {email}
-            </div>
-            <div className="col-start-2 col-end-3 p-2">
-              <strong>Phone Number:</strong> {phoneNumber}
-            </div>
-          </div>
-        </div>
-        <div className="absolute bottom-0 left-0 right-0">
-          <RegisterForm userId={userId} />
-        </div>
       </div>
-
   );
 }
 

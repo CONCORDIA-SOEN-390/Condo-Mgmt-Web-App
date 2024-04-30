331b5aa5fd05d1d38d90a19b9a4cfa9a076a8ec2
BoltyZeus <74887519+BoltyZeus@users.noreply.github.com>
2024-04-30 15:48:32 -0400

    Add document route created

---
+++ next_project/src/app/api/addDocument/route.js
@@ -0,0 +1,35 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Initialize Supabase client
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
+export async function POST(req) {
+  const body = await req.json();
+  const { propertyId, link, title} = body;
+
+  try {
+    
+    const { data, error } = await supabase
+    .from('file_')
+    .insert([
+    {property_id: propertyId, file_data: link, title: title},
+    ])
+    .select();
+
+    if (error != null){
+      return new Response(JSON.stringify(error), {
+        status:500,
+      });
+    }
+    return new Response('Success',{
+      status:200,
+    });
+  } catch (error) {
+    return new Response('Internal Server Errror', {
+      status:500,
+    });
+  }
+}

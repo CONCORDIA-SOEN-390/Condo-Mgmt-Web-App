bd62a0a55abb9085dc4e6eda5c1b99b1177b2033
BoltyZeus <74887519+BoltyZeus@users.noreply.github.com>
2024-04-30 14:13:32 -0400

    Added supabase implementation to last methods using local db

--- next_project/src/app/api/getMonthlyRentByProperty/route.js
+++ next_project/src/app/api/getMonthlyRentByProperty/route.js
@@ -1,68 +1,29 @@
-import pool from "../../../../utils/db";
+import { createClient } from '@supabase/supabase-js';
 
+// Initialize Supabase client
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
 export async function POST(req) {
     const body = await req.json();
-    const { userId, propertyId } = body;
-
-    const client = await pool.connect();
+    const { propertyId } = body;
 
     try {
-        await client.query("BEGIN");
 
-        // check if property exists
-        const prop = await client.query("SELECT * FROM property WHERE user_id = $1 AND property_id = $2", [userId, propertyId]);
+        let { data: rental_income, error } = await supabase
+        .from('rental_income')
+        .select("*")
+        .eq('property_id', propertyId);
 
-        if (prop.rows.length === 0) {
-            return new Response("Error fetching property data", {
-                status: 404
+        if (error != null){
+            return new Response(JSON.stringify(error), {
+              status:500,
             });
         }
 
-        // get rows from the request table for propertyId  with type_name, status_name, and creator username
-        const reqs = await client.query(`
-        SELECT 
-            p.property_name,
-            p.address,
-            COALESCE(u.condo_fee, 0) + COALESCE(l.locker_fee, 0) + COALESCE(pr.parking_fee, 0) AS total_condo_fee
-        FROM 
-            property p
-        LEFT JOIN (
-            SELECT 
-                property_id,
-                SUM(condo_fee) AS condo_fee
-            FROM 
-                unit
-            GROUP BY 
-                property_id
-        ) u ON p.property_id = u.property_id
-        LEFT JOIN (
-            SELECT 
-                property_id,
-                SUM(CASE WHEN occupied THEN condo_fee ELSE 0 END) AS locker_fee
-            FROM 
-                locker
-            GROUP BY 
-                property_id
-        ) l ON p.property_id = l.property_id
-        LEFT JOIN (
-            SELECT 
-                property_id,
-                SUM(CASE WHEN occupied THEN condo_fee ELSE 0 END) AS parking_fee
-            FROM 
-                parking
-            GROUP BY 
-                property_id
-        ) pr ON p.property_id = pr.property_id
-        WHERE 
-            p.property_type = 'rental'
-            AND p.property_id = $1;
-        `, [propertyId]);
-
-        // commit the transaction
-        await client.query("COMMIT");
-
         // return json string to client side
-        return new Response(JSON.stringify(reqs.rows[0]), {
+        return new Response(JSON.stringify(rental_income), {
             status: 200,
             headers: {
                 'Content-Type': 'application/json'
@@ -70,12 +31,9 @@ export async function POST(req) {
         });
 
     } catch (error) {
-        await client.query("ROLLBACK"); // Rollback the transaction if an error occurs
         console.error("Error getting data from tables:", error);
         return new Response('Internal Server Error', {
             status: 500
         });
-    } finally {
-        client.release();
     }
 }
--- next_project/src/app/api/getRentalIncomeByCompany/route.js
+++ next_project/src/app/api/getRentalIncomeByCompany/route.js
@@ -1,55 +1,29 @@
-import pool from "../../../../utils/db";
+import { createClient } from '@supabase/supabase-js';
 
+// Initialize Supabase client
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
 export async function POST(req) {
     const body = await req.json();
     const { userId } = body;
 
-    const client = await pool.connect();
-
     try {
-        const reqs = await client.query(`
-        SELECT 
-            p.property_id,
-            p.property_name,
-            p.address,
-            COALESCE(u.condo_fee, 0) + COALESCE(l.locker_fee, 0) + COALESCE(pr.parking_fee, 0) AS total_condo_fee
-        FROM 
-            property p
-        LEFT JOIN (
-            SELECT 
-                property_id,
-                SUM(condo_fee) AS condo_fee
-            FROM 
-                unit
-            GROUP BY 
-                property_id
-        ) u ON p.property_id = u.property_id
-        LEFT JOIN (
-            SELECT 
-                property_id,
-                SUM(CASE WHEN occupied THEN condo_fee ELSE 0 END) AS locker_fee
-            FROM 
-                locker
-            GROUP BY 
-                property_id
-        ) l ON p.property_id = l.property_id
-        LEFT JOIN (
-            SELECT 
-                property_id,
-                SUM(CASE WHEN occupied THEN condo_fee ELSE 0 END) AS parking_fee
-            FROM 
-                parking
-            GROUP BY 
-                property_id
-        ) pr ON p.property_id = pr.property_id
-        WHERE 
-            p.user_id = $1
-            AND p.property_type = 'rental';
-        `, [userId]);
 
+        let { data: rental_income, error } = await supabase
+        .from('rental_income')
+        .select("*")
+        .eq('user_id', userId);
+
+        if (error != null){
+            return new Response(JSON.stringify(error), {
+              status:500,
+            });
+        }
 
         // return json string to client side
-        return new Response(JSON.stringify(reqs.rows), {
+        return new Response(JSON.stringify(rental_income), {
             status: 200,
             headers: {
                 'Content-Type': 'application/json'
@@ -57,10 +31,9 @@ export async function POST(req) {
         });
 
     } catch (error) {
+        console.error("Error getting data from tables:", error);
         return new Response('Internal Server Error', {
             status: 500
         });
-    } finally {
-        client.release();
     }
 }

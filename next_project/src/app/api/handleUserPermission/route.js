5cabd12960be868bfab15bb2bf5317e042a1a88a
Racha Kara <78180153+r-kara@users.noreply.github.com>
2024-05-02 00:14:25 -0400

    Revert "Supabase finish"

--- next_project/src/app/api/handleUserPermission/route.js
+++ next_project/src/app/api/handleUserPermission/route.js
@@ -1,21 +1,14 @@
-import { createClient } from '@supabase/supabase-js';
+import pool from "../../../utils/db";
 
-// Initialize Supabase client
-const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  );
 export async function POST(req) {
     const body = await req.json();
     const { userId, pageType } = body;
+    const client = await pool.connect();
     let allowAccess = false;
 
     try {
-        let { data: user, error } = await supabase
-        .from('users')
-        .select("*")
-        .eq('user_id', userId);
-        const account_type= user[0]['account_type'];
+        const user = await client.query("SELECT * FROM users WHERE user_id = $1", [userId]);
+        const account_type= user.rows[0]['account_type'];
         switch (account_type){
             case 'company':
                 allowAccess = true;
@@ -46,5 +39,7 @@ export async function POST(req) {
         return new Response('Internal Server Error',{
             status:500,
         });
+    } finally {
+        client.release();
     }
 }
